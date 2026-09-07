2ab34066697ccf0e1f917babbc75ac35ababeea1
jdalton <john.david.dalton@gmail.com>
2026-09-07 10:16:40 -0400

    fix(external): expose inquirer separator to native esm

--- src/external/@inquirer/checkbox.js
+++ src/external/@inquirer/checkbox.js
@@ -2,4 +2,6 @@
 
 // Re-export from external-pack bundle for better deduplication.
 const { checkbox } = require('../external-pack')
+// Declare the named export for Node's CommonJS lexer before forwarding the namespace.
+exports.Separator = checkbox.Separator
 module.exports = checkbox
--- src/external/@inquirer/search.js
+++ src/external/@inquirer/search.js
@@ -2,4 +2,6 @@
 
 // Re-export from external-pack bundle for better deduplication.
 const { search } = require('../external-pack')
+// Declare the named export for Node's CommonJS lexer before forwarding the namespace.
+exports.Separator = search.Separator
 module.exports = search
